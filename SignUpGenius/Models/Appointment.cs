30db4b2e5d15eb3bb721e237d352604da0f26653
cjtracy <connertracy@comcast.net>
2022-03-19 18:25:12 -0600

    Home Page and started Model for appointment.

---
+++ SignUpGenius/Models/Appointment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignUpGenius.Models
+{
+    public class Appointment
+    {
+        [Key]
+        [Required]
+        public int AppointmentId { get; set; }
+
+        [Required]
+        public DateTime ApptTime { get; set; }
+
+        [Required]
+        public string GroupName { get; set; }
+
+        [Required]
+        public byte GroupSize { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        [Phone(ErrorMessage = "Type a valid 10 digit phone number")]
+        public string Phone { get; set; }
+    }
+}
