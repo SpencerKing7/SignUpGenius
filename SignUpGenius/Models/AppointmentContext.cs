3c82d11f5b678e3ef009b9e7ae679845617d9401
SpencerKing7 <spencerking.c@gmail.com>
2022-03-21 15:40:49 -0600

    css on form, wrote code for database build

---
+++ SignUpGenius/Models/AppointmentContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignUpGenius.Models
+{
+    public class AppointmentContext : DbContext
+    {
+        public AppointmentContext()
+        {
+        }
+
+        public AppointmentContext(DbContextOptions<AppointmentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
