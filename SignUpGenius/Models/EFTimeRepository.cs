5759d4a669e78a06abe8f6e80bf6e78a85bf465e
SpencerKing7 <spencerking.c@gmail.com>
2022-03-21 20:18:45 -0600

    changes that will probably be changed

---
+++ SignUpGenius/Models/EFTimeRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignUpGenius.Models
+{
+    public class EFTimeRepository : ITimeRepository
+    {
+        private AppointmentContext context { get; set; }
+
+        public EFTimeRepository (AppointmentContext temp)
+        {
+            context = temp;
+        }
+
+        public IQueryable<AppointmentTime> Times => context.AppointmentTimes;
+    }
+}
